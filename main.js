66e8187ffe538de323de5305a8d47f2cebfa47e1
Olof Vedung <vedungolof@gmail.com>
2024-08-02 19:26:01 +0200

    Fix mobile share-bar

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const shareButton = document.getElementById("share-button")
+const darkShareButton = document.getElementById("dark-share-button")
 const shareBar = document.getElementById("share-bar");
 
 shareButton.addEventListener("click", () => {
     shareBar.classList.toggle("display-none");
+
+    if (window.innerWidth < 800) {
+        darkShareButton.classList.toggle("display-none");   
+    }
+})
+
+darkShareButton.addEventListener("click", () => {
+    shareBar.classList.toggle("display-none");
+
+    if (window.innerWidth < 800) {
+        darkShareButton.classList.toggle("display-none");   
+    }
 })
